b066ca2c660738fb1890a8d8a1b07767e7561060
Emil Koutanov <ekoutanov@gmail.com>
2022-09-09 15:06:14 +1000

    Added 32-bit test

--- tinyrand-std/src/lib.rs
+++ tinyrand-std/src/lib.rs
@@ -6,3 +6,15 @@ pub mod thread_local;
 pub use clock_seed::ClockSeed;
 pub use thread_local::ThreadLocalRand;
 pub use thread_local::thread_rand;
+
+#[cfg(test)]
+mod tests {
+    /// All this does is print the pointer width. Useful for determining the `usize` width
+    /// of the current platform.
+    #[test]
+    fn pointer_width() {
+        println!("{}-bit", num_bits::<usize>());
+    }
+
+    const fn num_bits<T>() -> usize { std::mem::size_of::<T>() * 8 }
+}
