07ede7c8550bebf5daeb6cf9864903e8939bf732
Emil Koutanov <ekoutanov@gmail.com>
2022-09-10 13:28:24 +1000

    Bonferroni correction for multiple Bernoulli trials

--- tinyrand/tests/bernoulli_trials.rs
+++ tinyrand/tests/bernoulli_trials.rs
@@ -5,25 +5,24 @@
 //! random source, subjected to sufficient number of trials, will fail some of them. The
 //! number of failed trials should, therefore, remain within the confidence level.
 
-use std::any;
 use rand::rngs::StdRng;
 use rand::{RngCore, SeedableRng};
+use std::any;
 use tinyrand::{Counter, Probability, Rand, RandRange, Seeded, Wyrand, Xorshift};
 
 #[test]
 fn bernoulli_trials_wyrand() {
-    bernoulli_trials::<Wyrand>(Options::default());
+    bernoulli_trials_bc::<Wyrand>(Options::default()).unwrap();
 }
 
 #[test]
 fn bernoulli_trials_xorshift() {
-    bernoulli_trials::<Xorshift>(Options::default());
+    bernoulli_trials_bc::<Xorshift>(Options::default()).unwrap();
 }
 
 #[test]
-#[should_panic(expected="rejected H0")]
-fn bernoulli_trials_counter() {
-    bernoulli_trials::<Counter>(Options::default());
+fn bernoulli_trials_counter_should_reject() {
+    assert!(bernoulli_trials_bc::<Counter>(Options::default()).is_err());
 }
 
 #[derive(Debug)]
@@ -34,17 +33,17 @@ struct Options {
     // Experiments per trial.
     iters: u16,
 
-    // Confidence to reject H0 (stream is random). The lower the confidence, the more likely
+    // Significance level to reject H0 (stream is random). The higher the significance level, the more likely
     // H1 (stream is nonrandom) is accepted.
-    confidence_level: f64,
+    significance_level: f64,
 }
 
 impl Default for Options {
     fn default() -> Self {
         Self {
-            trials: 1000,
+            trials: 1_000,
             iters: 30,
-            confidence_level: 0.90,
+            significance_level: 0.25,
         }
     }
 }
@@ -53,19 +52,30 @@ impl Options {
     fn validate(&self) {
         assert!(self.trials > 0);
         assert!(self.iters > 0);
-        assert!(self.confidence_level >= f64::EPSILON);
-        assert!(self.confidence_level <= 1.0 - f64::EPSILON);
+        assert!(self.significance_level >= f64::EPSILON);
+        assert!(self.significance_level <= 1.0 - f64::EPSILON);
     }
 }
 
-fn bernoulli_trials<S: Seeded>(opts: Options)
+#[derive(Debug)]
+pub struct Rejection {
+    /// The significance level at which H0 was rejected.
+    pub alpha: f64,
+
+    /// The p-value of the test.
+    pub p_value: f64,
+}
+
+/// Runs a series of Bernoulli trials using Bonferroni correction to depress the Type I error rate.
+fn bernoulli_trials_bc<S: Seeded>(opts: Options) -> Result<(), Vec<Rejection>>
 where
     S::R: RandRange<u64>,
 {
     opts.validate();
     let rand_type = any::type_name::<S>();
     let mut control_rng = StdRng::seed_from_u64(0);
-    let mut rejections = 0;
+    let alpha = opts.significance_level / f64::from(opts.trials);
+    let mut rejections = vec![];
 
     for trial in 0..opts.trials {
         let seed = control_rng.next_u64();
@@ -78,18 +88,30 @@ where
                 heads += 1;
             }
         }
-        let run_within_prob = integrate_outcome_probs(opts.iters, weight,heads);
+        let run_within_prob = integrate_outcome_probs(opts.iters, weight, heads);
         let p_value = 1.0 - run_within_prob;
 
-        if 1.0 - opts.confidence_level > p_value {
-            println!("[{rand_type} trial {trial}] rejected H0: {heads} heads from {} runs at weight of {weight}, p={p_value}", opts.iters);
-            rejections += 1;
+        if p_value < alpha {
+            println!("[{rand_type} trial {trial}] rejected H0: {heads} heads from {} runs at weight of {weight}, p={p_value} < {alpha}", opts.iters);
+            rejections.push(p_value);
         }
     }
 
-    let p_value = f64::from(rejections) / f64::from(opts.trials);
-    println!("[{rand_type}] rejections/trials: {rejections}/{}, p={p_value}", opts.trials);
-    assert!(p_value < 1.0 - opts.confidence_level, "rejected H0: rejections/cycles: {rejections}/{}, p={p_value}", opts.trials);
+    println!(
+        "[{rand_type}] rejections/trials: {}/{}",
+        rejections.len(),
+        opts.trials
+    );
+
+    if rejections.is_empty() {
+        Ok(())
+    } else {
+        let rejections = rejections
+            .into_iter()
+            .map(|p_value| Rejection { alpha, p_value })
+            .collect();
+        Err(rejections)
+    }
 }
 
 fn generate_weight_for_test(rng: &mut StdRng) -> f64 {
@@ -104,12 +126,13 @@ fn generate_weight_for_test(rng: &mut StdRng) -> f64 {
 ///
 /// `n` — number of experiments in the sequence.
 /// `w` — probability of success (equivalently, weight of the coin, where `w` > 0.5 is biased towards heads).
-fn integrate_outcome_probs(n: u16, w: f64, k: u16) -> f64{
+fn integrate_outcome_probs(n: u16, w: f64, k: u16) -> f64 {
     let outcome_prob = bernoulli_pmf(k, n, w);
     (0..=n)
         .map(|k| bernoulli_pmf(k, n, w))
         .filter(|&p| p > outcome_prob)
-        .sum()
+        .sum::<f64>()
+        .min(1.0)
 }
 
 /// Obtains the Bernoulli Probability Mass Function.
